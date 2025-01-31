a9e15c5877a78fca2612b1456bd3a42020b23cc0
dvid-arch <derrickemma44@gmail.com>
2025-01-31 11:25:40 +0100

    made changes

--- app/routes/flashcard._index/index.tsx
+++ app/routes/flashcard._index/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@remix-run/react"
 
 function index() {
   return (
@@ -6,9 +7,9 @@ function index() {
             <h2 className="text-3xl font-bold"> Pick up Where you left off</h2>
             
         </div>
-        <div className="h-80 w-80 border bg-blue-500 border-pink-500 py-6 px-6 relative">
+        <div className="h-80 w-80 border bg-blue-500 border-pink-500 rounded-md py-6 px-6 relative flex flex-col justify-between">
             <h2 className="text-3xl font-bold">Study Now to Create a flashCard</h2>
-            
+            <Link to={'/study'}className="bg-white text-blue-500 block py-4 px-8 w-fit rounded-full">Study Now</Link>
         </div>
     </div>
   )
